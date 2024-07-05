d688db5a02736a1540a1f7bda211b18863f74bb5
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-07-05 13:45:24 +0300

    Initial commit

---
+++ MultiplyEvensbyOdds/Program.cs
@@ -0,0 +1,56 @@
+﻿class MultiplyEvensByOdds
+{
+    static void Main()
+    {
+        
+        int number = int.Parse(Console.ReadLine());
+
+        int sumOfEvens = GetSumOfEvenDigits(number);
+        int sumOfOdds = GetSumOfOddDigits(number);
+
+        int result = GetMultipleOfEvenAndOdds(sumOfEvens, sumOfOdds);
+
+        Console.WriteLine($"{result}");
+    }
+
+    static int GetSumOfEvenDigits(int number)
+    {
+        int sum = 0;
+        number = Math.Abs(number);
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            if (digit % 2 == 0)
+            {
+                sum += digit;
+            }
+            number /= 10;
+        }
+
+        return sum;
+    }
+
+    static int GetSumOfOddDigits(int number)
+    {
+        int sum = 0;
+        number = Math.Abs(number);
+
+        while (number > 0)
+        {
+            int digit = number % 10;
+            if (digit % 2 != 0)
+            {
+                sum += digit;
+            }
+            number /= 10;
+        }
+
+        return sum;
+    }
+
+    static int GetMultipleOfEvenAndOdds(int sumOfEvens, int sumOfOdds)
+    {
+        return sumOfEvens * sumOfOdds;
+    }
+}
